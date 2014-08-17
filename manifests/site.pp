03becaff75a1428289759221087545da4475a502
Les Aker <me@lesaker.org>
2014-08-17 14:12:31 -0400

    first baby step

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,8 @@ Exec {
     "${boxen::config::home}/rbenv/bin",
     "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
+    '/usr/local/bin',
+    '/usr/local/sbin',
     '/usr/bin',
     '/bin',
     '/usr/sbin',
@@ -52,40 +54,15 @@ Service {
 Homebrew::Formula <| |> -> Package <| |>
 
 node default {
-  # core modules, needed for most things
-  include dnsmasq
   include git
-  include hub
-  include nginx
+  #include filevault
+  include ruby
 
-  # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
-
-  # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
-
-  # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
-
-  # common, useful packages
   package {
     [
       'ack',
       'findutils',
       'gnu-tar'
     ]:
-  }
-
-  file { "${boxen::config::srcdir}/our-boxen":
-    ensure => link,
-    target => $boxen::config::repodir
   }
 }
