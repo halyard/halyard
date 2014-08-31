c2c5ffc77cd408505c700cfdc7d60e5d13b7497a
Les Aker <me@lesaker.org>
2014-08-30 22:00:32 -0400

    set shell to zsh

--- manifests/site.pp
+++ manifests/site.pp
@@ -222,4 +222,15 @@ node default {
     ],
     type     => 'user_spec',
   }
+
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+
+  osx_chsh { $::luser:
+    shell   => "${boxen::config::homebrewdir}/bin/zsh",
+    require => File_line['add zsh to /etc/shells'],
+  }
 }
