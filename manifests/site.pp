5793deb3c289e0442f9106995d17f9b4be33b6cf
Les Aker <me@lesaker.org>
2014-08-31 11:20:44 -0400

    pop packages into separate manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,4 @@
 require boxen::environment
-require homebrew
-require brewcask
 
 Exec {
   group       => 'staff',
@@ -32,12 +30,6 @@ File {
   owner => $boxen_user
 }
 
-Package {
-  provider => homebrew,
-  require  => Class['homebrew'],
-  install_options => ['--build-from-source']
-}
-
 Repository {
   provider => git,
   extra    => [
@@ -53,11 +45,11 @@ Service {
   provider => ghlaunchd
 }
 
-Homebrew::Formula <| |> -> Package <| |>
-
 node default {
   include git
   include ruby
+
+  import 'packages'
 
   include osx::global::disable_key_press_and_hold
   include osx::global::expand_save_dialog
@@ -95,10 +87,6 @@ node default {
     bottom_left => 'Dashboard'
   }
 
-  homebrew::tap { 'homebrew/dupes': }
-  homebrew::tap { 'halyard/formulae': }
-  homebrew::tap { 'halyard/casks': }
-
   ruby::version { '1.8.7':
     require     => Package['apple-gcc42']
   }
@@ -109,154 +97,6 @@ node default {
   ruby::version { '2.1.2': }
   class { 'ruby::global':
     version => '2.1.2'
-  }
-
-  package {
-    [
-      'ack',
-      'arping',
-      'bash',
-      'bash-completion',
-      'coreutils',
-      'cpanminus',
-      'ctags',
-      'findutils',
-      'gawk',
-      'git',
-      'gnupg',
-      'gnupg2',
-      'gnuplot',
-      'gnutls',
-      'gnu-tar',
-      'go',
-      'gpg-agent',
-      'gpgme',
-      'htop-osx',
-      'ipmitool',
-      'mutt',
-      'mysql',
-      'netcat',
-      'nmap',
-      'node',
-      'openssl',
-      'pinentry',
-      'pwgen',
-      'python',
-      'python3',
-      'qrencode',
-      'readline',
-      'redis',
-      'siege',
-      'socat',
-      'sqlite',
-      'swig',
-      'tmux',
-      'tree',
-      'vim',
-      'watch',
-      'weechat',
-      'wget',
-      'xz',
-      'yuicompressor',
-      'zsh',
-      'zsh-completions'
-    ]:
-  }
-
-  package { 'mtr':
-    install_options => [
-      '--build-from-source',
-      '--no-gtk+'
-    ]
-  }
-
-  file { 'mtr-binary':
-    path    => "${boxen::config::home}/homebrew/sbin/mtr",
-    links   => 'follow',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '4755',
-    require => Package['mtr']
-  }
-
-  package {
-    [
-      'halyard/formulae/encfs',
-      'halyard/formulae/openssh',
-      'halyard/formulae/ipmiutil'
-    ]:
-    require => [
-      Homebrew::Tap['halyard/formulae'],
-      Package['halyard/casks/osxfuse']
-    ]
-  }
-
-  package {
-    [
-      'apple-gcc42',
-      'grep',
-      'screen'
-    ]:
-    require => Homebrew::Tap['homebrew/dupes']
-  }
-
-  package {
-    [
-      'halyard/casks/airmail-beta',
-      'halyard/casks/alfred',
-      'halyard/casks/asepsis',
-      'halyard/casks/bartender',
-      'halyard/casks/bettertouchtool',
-      'halyard/casks/cert-quicklook',
-      'halyard/casks/copy',
-      'halyard/casks/dropbox',
-      'halyard/casks/firefox-beta',
-      'halyard/casks/flux',
-      'halyard/casks/font-meslo-lg-for-powerline',
-      'halyard/casks/google-chrome-dev',
-      'halyard/casks/google-drive',
-      'halyard/casks/gpgtools',
-      'halyard/casks/grandperspective',
-      'halyard/casks/hyperspace',
-      'halyard/casks/jre',
-      'halyard/casks/istat-menus',
-      'halyard/casks/iterm2-nightly',
-      'halyard/casks/lingon-x',
-      'halyard/casks/little-snitch-nightly',
-      'halyard/casks/luxdelux',
-      'halyard/casks/navicat-for-mysql',
-      'halyard/casks/onepassword-beta',
-      'halyard/casks/openoffice',
-      'halyard/casks/osxfuse',
-      'halyard/casks/quicklook-json',
-      'halyard/casks/rstudio',
-      'halyard/casks/sequelpro',
-      'halyard/casks/textexpander',
-      'halyard/casks/the-unarchiver',
-      'halyard/casks/thunderbird-beta',
-      'halyard/casks/totalfinder',
-      'halyard/casks/totalspaces',
-      'halyard/casks/transmission',
-      'halyard/casks/vagrant',
-      'halyard/casks/virtualbox-ext-pack',
-      'halyard/casks/virtualbox',
-      'halyard/casks/vlc',
-      'halyard/casks/xee'
-    ]:
-    provider    => 'brewcask',
-    require     => [
-      Sudoers['brewcask-pkginstaller'],
-      Package['grep']
-    ]
-  }
-
-  sudoers { 'brewcask-pkginstaller':
-    users       => $::boxen_user,
-    hosts       => 'ALL',
-    commands    => [
-      '(ALL) NOPASSWD:SETENV: /usr/sbin/installer',
-    ],
-    type        => 'user_spec',
   }
 
   file_line { 'add zsh to /etc/shells':
