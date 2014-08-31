10bc101767cba906381349b22ea1b8f5ef36f1be
Les Aker <me@lesaker.org>
2014-08-30 21:40:35 -0400

    add homebrew sbin

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,7 @@ Exec {
     "${boxen::config::home}/rbenv/bin",
     "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
+    "${boxen::config::home}/homebrew/sbin",
     '/usr/local/bin',
     '/usr/local/sbin',
     '/usr/bin',
