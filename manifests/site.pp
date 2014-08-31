f5724799ab8e6fb5f950c021c279b753feea53d1
Les Aker <me@lesaker.org>
2014-08-31 16:56:10 -0400

    move zsh config to puppet-zsh

--- manifests/site.pp
+++ manifests/site.pp
@@ -60,15 +60,5 @@ node default {
   include hostname
   include fdesetup
   include dotdotdot
-
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
-
-  osx_chsh { $::luser:
-    shell   => "${boxen::config::homebrewdir}/bin/zsh",
-    require => File_line['add zsh to /etc/shells'],
-  }
+  include zsh
 }
