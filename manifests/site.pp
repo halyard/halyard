435c60bbbbed29e77fee8c7bc125b0e7fee18c92
Les Aker <me@lesaker.org>
2014-08-30 22:57:17 -0400

    don't use template

--- manifests/site.pp
+++ manifests/site.pp
@@ -283,7 +283,7 @@ node default {
     group   => 'staff',
     mode    => '0644',
     require => Repository['dotdotdot repo'],
-    content => template('dotdotdot.conf')
+    content => "dots:\n- path: dotfiles\n- path: privdotfiles"
   }
 
   exec { 'dotdotdot update':
