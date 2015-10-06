c03adb939088fe90562d267105dd31972ce0c864
Les Aker <me@lesaker.org>
2015-10-06 12:10:57 -0400

    use puppet forge for versioning

--- meta/update.rb
+++ meta/update.rb
@@ -2,32 +2,27 @@
 
 require 'json'
 require 'open-uri'
-require 'octoauth'
-require 'octokit'
 
-path = 'Puppetfile'
-regex = /^halyard '(?<name>\w+)', '(?<version>[\w.]+)'$/
+PATH = 'Puppetfile'
+REGEX = /^halyard '(?<name>\w+)', '(?<version>[\w.]+)'$/
+URL = 'https://forgeapi.puppetlabs.com/v3/modules/halyard-%s'
 
-Auth = Octoauth.new note: 'halyard', file: :default, autosave: true
-Client = Octokit::Client.new(access_token: Auth.token, auto_paginate: true)
-
-def latest_release(name, old)
-  Client.latest_release("halyard/puppet-#{name}").tag_name
-rescue Octokit::NotFound
-  puts "No releases for #{name}"
-  old
+def latest_release(name)
+  JSON.load(open(URL % name)).fetch('current_release', {})['version']
+rescue OpenURI::HTTPError
+  puts "No release found for #{name}"
 end
 
 def update(match)
   _, name, old = match.to_a
-  latest = latest_release(name, old)
+  latest = latest_release(name) || old
   puts "Updating #{name} from #{old} to #{latest}" if old != latest
   "halyard '#{name}', '#{latest}'"
 end
 
-lines = File.read(path).split("\n").map do |line|
-  match = line.match(regex)
+lines = File.read(PATH).split("\n").map do |line|
+  match = line.match(REGEX)
   match ? update(match) : line
 end
 
-File.open(path, 'w') { |fh| lines.each { |line| fh << "#{line}\n" } }
+File.open(PATH, 'w') { |fh| lines.each { |line| fh << "#{line}\n" } }
