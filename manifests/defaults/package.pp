1d9239429da22f48a1a1d5eef0489e12c528245b
Les Aker <me@lesaker.org>
2017-01-08 14:05:38 -0800

    comment out package defaults for now

--- manifests/defaults/package.pp
+++ manifests/defaults/package.pp
@@ -1,5 +1,5 @@
-Package {
-  provider        => homebrew,
-  require         => Class['homebrew'],
-  install_options => ['--build-from-source']
-}
+#Package {
+#  provider        => homebrew,
+#  require         => Class['homebrew'],
+#  install_options => ['--build-from-source']
+#}
