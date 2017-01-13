78267f555746a998ad22225b65b822c371d2c80c
Les Aker <me@lesaker.org>
2017-01-13 03:31:33 -0500

    switch to hiera for plugin def

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,3 @@
 include 'halyard'
-class { 'homebrew':
-  path => '/opt/brew'
-}
+include 'homebrew'
 include 'asdf'
-asdf::plugin { [
-  'https://github.com/asdf-vm/asdf-ruby',
-  'https://github.com/tuvistavie/asdf-python',
-  'https://github.com/kennyp/asdf-golang',
-  'https://github.com/asdf-vm/asdf-erlang',
-  'https://github.com/neerfri/asdf-terraform'
-]: }
